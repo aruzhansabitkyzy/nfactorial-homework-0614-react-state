16bd0c81030eec9cd472fd4e80a0f0ed516cd7a6
Aruzhan Sabitkyzy <arusabitkyzy03@mail.ru>
2022-06-15 19:27:08 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { v4 as myNewID } from "uuid";
 
 import "./App.css";
@@ -19,36 +19,39 @@ const buttons = [
   },
 ];
 
+
 function App() {
+  const local = ()  => {
+    let list = localStorage.getItem('todos')
+  
+    if (list) {
+      return JSON.parse(localStorage.getItem('todos'))
+    } else {
+      return []
+    }
+  }
+  
   const [itemToDo, setItemToDo] = useState("");
-  const [items, setItems] = useState([
-    {
-      key: 1,
-      label: "Have fun",
-    },
-    {
-      key: 2,
-      label: "Spread Empathy",
-    },
-    {
-      key: 3,
-      label: "Generate Value",
-    },
-  ]);
-
+  const [items, setItems] = useState(local);
   const [filterType, setFilterType] = useState("all");
-
+  const[filteredData, setFilteredData] = useState([]);
   const handleToDoChange = (event) => {
     setItemToDo(event.target.value);
   };
-
+ 
   const handleAddItem = () => {
     const newItem = { key: myNewID(), label: itemToDo };
 
     setItems((prevElement) => [newItem, ...prevElement]);
-
     setItemToDo("");
+    
+    localStorage.setItem("todos",JSON.stringify(newItem));
+    localStorage.getItem('todos')
   };
+
+  useEffect(()=> {
+    localStorage.setItem("todos",JSON.stringify(items));
+  }, [items]);
 
   const handleItemDone = ({ key }) => {
     setItems((prevItems) =>
@@ -60,20 +63,69 @@ function App() {
     );
   };
 
+  const handleImportant = ({key})  => {
+    setItems((prevItems) => prevItems.map((item) => {
+      if(item.key === key) {
+        return {...item, important: !item.important};
+      }else return item;
+    }))
+  }
+//   const handleImportant = ({key}) => {
+//     setItems((prevItems) => 
+//     prevItems.map((cur) => {
+//       if(cur.key === key) {
+//         if(cur.label.indexOf("important") == -1) {
+//           console.log(cur.label);
+//           return {...cur, label : cur.label + " important"} 
+//         }
+//         else if(cur.label.indexOf("important") != -1){
+//           return {...cur, label: cur.label.replace("important", "")}
+//         }
+//       }
+//       else return cur;
+//     }))
+// };
+
   const handleFilterChange = ({ type }) => {
     setFilterType(type);
   };
+  const handleSearchingBar = (event)=> {
+      let value = event.target.value; 
+
+      const todos = items.filter(todo => {
+        return todo.label.indexOf(value) > -1;
+      });
+
+      setFilterType("search");
+      setFilteredData(todos);
+  };
+ 
+  const handleDelete = ({key}) => {
+       setItems((prevItems) => {
+           return prevItems.filter((item)=> {
+                if(item.key != key) {
+                  localStorage.removeItem('todos', JSON.stringify(item));
+                  return true; 
+                }
+                return false;
+           });
+       });
+
+
+  }
+  
 
   const moreToDo = items.filter((item) => !item.done).length;
 
   const doneToDo = items.length - moreToDo;
 
-  const filteredArray =
-    filterType === "all"
-      ? items
-      : filterType === "done"
-      ? items.filter((item) => item.done)
-      : items.filter((item) => !item.done);
+  const filteredArray = filterType === "search" ? filteredData :
+  filterType === "all"
+                ? items
+                : filterType === "done"
+                ? items.filter((item) => item.done)
+                : items.filter((item) => !item.done) 
+    
 
   return (
     <div className="todo-app">
@@ -91,6 +143,7 @@ function App() {
           type="text"
           className="form-control search-input"
           placeholder="type to search"
+          onChange={event => (handleSearchingBar(event))}
         />
         {/* Item-status-filter */}
         <div className="btn-group">
@@ -116,21 +169,26 @@ function App() {
             <li key={item.key} className="list-group-item">
               <span className={`todo-list-item ${item.done ? "done" : ""}`}>
                 <span
-                  className="todo-list-item-label"
+                  className={`todo-list-item-label ${(item.important === true) ? "text-warning" : " "}`}
                   onClick={() => handleItemDone(item)}
                 >
                   {item.label}
+                  
                 </span>
 
-                <button
+                <button 
                   type="button"
-                  className="btn btn-outline-success btn-sm float-right"
+                  key={item.key}
+                  className={`btn btn-outline-success btn-sm float-right`}
+                  onClick={() => handleImportant(item)} 
                 >
                   <i className="fa fa-exclamation" />
                 </button>
 
                 <button
                   type="button"
+                  key = {item.id}
+                  onClick = {() => handleDelete(item)}
                   className="btn btn-outline-danger btn-sm float-right"
                 >
                   <i className="fa fa-trash-o" />
